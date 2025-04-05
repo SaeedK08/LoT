da21bc95da10a8f37b7d0dc98fdca5eecd156fd7
Saeed Kassab <saeedkassab2000@gmail.com>
2025-04-05 12:45:28 +0200

    Create a file for player.

---
+++ src/player.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#endif
+
+#define WINDOW_W 1920
+#define WINDOW_H 1080
+#define PLAYER_WIDTH 35
+#define PLAYER_HEIGHT 67
+
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <stdbool.h>
+
+bool init_player(SDL_Renderer* renderer);
+void render_player(SDL_Renderer* renderer);
+void update_player(void);
