b9bb59215c9d0cc5f69de4a1af8c2a26b21c5b26
Saeed Kassab <saeedkassab2000@gmail.com>
2025-04-25 20:26:09 +0200

    Small changes after merging conflict.

--- src/attack.c
+++ src/attack.c
@@ -2,7 +2,7 @@
 
 typedef struct FireBall {
     SDL_Texture* fireBallTex;
-    SDL_FPoint fireBallSrc;
+    SDL_FRect fireBallSrc;
     SDL_FPoint fireBallDst;
     SDL_FRect target;
     int hit;
@@ -26,7 +26,6 @@ void directFireBall() {
         fireBall.hit = 1;
         SDL_DestroyTexture(fireBall.fireBallTex);
         SDL_Log("HIT!!!!\n");
-        // SDL_Delay(10);
         return; 
     }
 }
@@ -42,7 +41,7 @@ bool renderFireBall(SDL_Renderer *renderer) {
 }
 
 void fireBallInit(SDL_Renderer *renderer, SDL_FPoint mousePos, SDL_FPoint player_position)  {
-    fireBall.fireBallSrc = (SDL_FPoint){64.0f, 26.0f};
+    fireBall.fireBallSrc = (SDL_FRect){0.0f, 0.0f, 33, 39};
     fireBall.fireBallDst = (SDL_FPoint) {player_position.x - camera.x - PLAYER_WIDTH/2, player_position.y - camera.y - PLAYER_HEIGHT/2};
     fireBall.target = (SDL_FRect){mousePos.x/4.26f,mousePos.y/4.26f,100.0f,100.0f};    // Scaling down mouse coordinates to camera view size
     SDL_Log("TARGET x: %f, y: %f", fireBall.target.x, fireBall.target.y);
